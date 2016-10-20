5dd206a69362af16437aab5a1fedace87e09676e
Adnan Siddiqi <kadnan@gmail.com>
2016-10-20 10:41:41 +0500

    Adding categories

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -13,6 +13,7 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         $result = DB::table('categories')->truncate();
+
         $category = new Category();
         $category->name = 'Agriculture';
         $category->parent = 0;
@@ -21,11 +22,108 @@ class CategoryTableSeeder extends Seeder
         $result = DB::table('categories')->where('name', 'Agriculture')->first();
         $new_category_id = $result->id;
 
-        /**
-         * Add Subcategory of Agriculture
-         */
+
         $category = new Category();
         $category->name = 'Agricultural Greenhouses';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Animal husbandry';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Oils & Extracts';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Fruits';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Vegetables';
+        $category->parent = $new_category_id;
+        $category->save();
+
+
+        $category = new Category();
+        $category->name = 'Apparel & Clothing';
+        $category->parent = 0;
+        $category->save();
+
+        $result = DB::table('categories')->where('name', 'Apparel & Clothing')->first();
+        $new_category_id = $result->id;
+
+        $category = new Category();
+        $category->name = 'Womens Clothing';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Garment Accessories';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Sportswear';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'T-Shirts';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Dresses';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Coats';
+        $category->parent = $new_category_id;
+        $category->save();
+
+
+        $category = new Category();
+        $category->name = 'Automobiles';
+        $category->parent = 0;
+        $category->save();
+
+        $result = DB::table('categories')->where('name', 'Automobiles')->first();
+        $new_category_id = $result->id;
+
+
+        $category = new Category();
+        $category->name = 'Auto Parts';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Auto Electronics';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Auto Engine System';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Auto Accessories';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Motorcycle & Bikes';
+        $category->parent = $new_category_id;
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Auto Electrical System';
         $category->parent = $new_category_id;
         $category->save();
 
