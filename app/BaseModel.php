2bdfbeb4fa8d5cfbcc4df85b01d16129ea206a17
Adnan Siddiqi <kadnan@gmail.com>
2016-11-06 22:28:04 +0500

    adding methods

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    public function scopegetLatest($query,$limit)
+    public function scopegetLatest($query, $limit)
     {
-        return $query->orderBy('created_at', 'desc')->take($limit)->select(['id','title'])->get();
+        return $query->orderBy('created_at', 'desc')->take($limit)->select(['id', 'title'])->get();
+    }
+
+    public function scopegetItemsWithImage($query, $limit)
+    {
+        return $query->where('is_featured', 1)->take($limit)->select(['id', 'title','images'])->get();
     }
 }
--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public static function getParentCategories()
+    {
+        return self::where('parent',0)->get();
+    }
 }
